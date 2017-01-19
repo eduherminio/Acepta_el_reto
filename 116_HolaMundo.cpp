0630aa0113950d1da8cb944fccfa2ca544487e89
Eduardo Caceres <edu_kueyar@hotmail.com>
2017-01-19 11:34:36 +0100

    Fix .gitignore

--- 116_HolaMundo.cpp
+++ 116_HolaMundo.cpp
@@ -1,37 +1,11 @@
 #include <iostream>
-#include <vector>
 
 using namespace std;
 
 int main()
 {
-	long long int n;
-	int resto;
-	vector<int> v;
-	string str;
-
-	while(true)
-	{
-		cin>>n;
-		
-		if(n==-1)
-			break;
-
-		while(resto= n%10)
-		{
-			v.push_back(resto);
-			n=resto;
-		}
-
-		for(int i=v.size()-1; i>-1; i--)
-		{
-			str+= v[i] << " + "
-		}
-
-		string output= str.substr(0, str.size()-3);
-
-		cout<<output;
-	}
+	int n_inputs;
+	cin>>n_inputs;
 
 	for(int i=0; i<n_inputs; i++)
 	{
