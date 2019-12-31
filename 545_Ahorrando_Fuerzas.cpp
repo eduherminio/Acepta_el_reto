e2bfd27e53e7e9cbb89560c6a2c12e2615822f6f
Eduardo Caceres <eduardo.caceresdelacalle@gmail.com>
2019-12-31 14:40:59 +0000

    Add 545 solved

---
+++ 545_Ahorrando_Fuerzas.cpp
@@ -0,0 +1,45 @@
+﻿#include <iostream>
+
+#include <string>   // string(str, i, length), stoi(str), to_string(num), getline(cin, str)
+#include <algorithm> // (see below) find, min, max, reverse, rotate...
+#include <vector>
+#include <list>
+
+int main()
+{
+	int long_trayecto;
+	while (std::cin >> long_trayecto)
+	{
+		int n_veh;
+		std::cin >> n_veh;
+		auto t_a_x0__speed = std::list<std::pair<int, int>>();
+
+		for (int i = 0; i < n_veh; ++i)
+		{
+			int t_at_x0, v;
+			std::cin >> t_at_x0;
+			std::cin >> v;
+
+			if (t_at_x0 >= 0)
+			{
+				t_a_x0__speed.push_back(std::make_pair(t_at_x0, v));
+			}
+		}
+
+		t_a_x0__speed.sort(
+			[](std::pair<int, int> a, std::pair<int, int> b) { return a.second > b.second; });
+
+		auto calculate_t = [long_trayecto](std::pair<int, int> pair)
+		{
+			return pair.first + long_trayecto / pair.second;
+		};
+
+		t_a_x0__speed.sort([calculate_t](
+			std::pair<int, int> a, std::pair<int, int> b)
+		{
+			return calculate_t(a) < calculate_t(b);
+		});
+
+		std::cout << calculate_t(*(t_a_x0__speed.begin())) << std::endl;
+	}
+}
